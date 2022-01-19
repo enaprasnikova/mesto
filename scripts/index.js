174610ff8e4270413a9b46ef40e74eacaa1e5814
Elena Naprasnikova <ev.naprasnikova@gmail.com>
2022-01-19 22:36:15 +0300

    выполненая работа

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+let editButton = document.querySelector('.profile__edit-button');
+editButton.addEventListener('click', openPopUp);
+
+let closeButton = document.querySelector('.popup__close-button');
+closeButton.addEventListener('click', closePopUp);
+
+let popUpForm = document.querySelector('.popup__input');
+popUpForm.addEventListener('submit', submitInfo);
+
+function submitInfo(evt) {
+  evt.preventDefault();
+  let inputNameElement = document.querySelector('.popup__input-text_name');
+  let inputAboutMeElement = document.querySelector('.popup__input-text_about-me');
+  let profileNameElement = document.querySelector('.profile__name');
+  let profileAboutMeElement = document.querySelector('.profile__about-me');
+  profileNameElement.textContent = inputNameElement.value;
+  profileAboutMeElement.textContent = inputAboutMeElement.value;
+  closePopUp();
+}
+
+function openPopUp() {
+  let popUp = document.querySelector('.popup');
+  popUp.classList.add('popup__popup_opened');
+  let profileNameElement = document.querySelector('.profile__name');
+  let profileAboutMeElement = document.querySelector('.profile__about-me');
+  let profileName = profileNameElement.textContent;
+  let profileAboutMe = profileAboutMeElement.textContent;
+  let inputName = popUp.querySelector('.popup__input-text_name');
+  let inputAboutMe = popUp.querySelector('.popup__input-text_about-me');
+  inputName.value = profileName;
+  inputAboutMe.value = profileAboutMe; 
+}
+
+function closePopUp() {
+  let popUp = document.querySelector('.popup');
+  popUp.classList.remove('popup__popup_opened');
+}
+
+
